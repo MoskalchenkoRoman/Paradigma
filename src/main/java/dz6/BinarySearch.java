5e742afba78023def5f9171d23d4c923f9222521
Роман <110688570+MoskalchenkoRoman@users.noreply.github.com>
2023-09-04 09:35:11 +0300

    dz6

---
+++ src/main/java/dz6/BinarySearch.java
@@ -0,0 +1,25 @@
+package dz6;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int valueToFind = 7;
+
+        System.out.printf("Index = %d%n", binarySearch(values, valueToFind, 0, values.length - 1));
+    }
+
+    private static int binarySearch(int[] values, int valueToFind, int l, int r) {
+        if (l == r) {
+            return (values[l] == valueToFind) ? l : -1;
+        }
+
+        int m = l + (r - l) / 2;
+
+        if (valueToFind > values[m]) {
+            return binarySearch(values, valueToFind, m + 1, r);
+        } else if (values[m] > valueToFind) {
+            return binarySearch(values, valueToFind, l, m - 1);
+        }
+        return m;
+    }
+}
